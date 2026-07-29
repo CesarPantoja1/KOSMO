9e2f1ee1b0187970411e23720721fdd87ad4349a
Jefferson Chileno <jefferson.chileno@epn.edu.ec>
2026-07-29 17:22:42 -0500

    feat: mejora de estilos en pantallas y chat

--- frontend/src/pages/project/CreateProjectPage/ui/CreateProjectPage.tsx
+++ frontend/src/pages/project/CreateProjectPage/ui/CreateProjectPage.tsx
@@ -3,7 +3,7 @@ import { CreateProjectForm } from './CreateProjectForm';
 const CreateProjectPage = () => {
 	return (
 		<div className='page-container'>
-			<div className='page-header'>
+			<div className='page-header mb-4'>
 				<h1 className='text-base-800 text-3xl font-bold'>Crear Proyecto</h1>
 				<p className='text-base-600 text-lg'>
 					Define la idea central y los objetivos de tu aplicación. Una descripción clara y
@@ -11,8 +11,8 @@ const CreateProjectPage = () => {
 					del desarrollo: desde la descripción general y la extracción de características,
 					hasta el desglose de requisitos, el modelado y la generación de código base.
 				</p>
+				<CreateProjectForm />
 			</div>
-			<CreateProjectForm />
 		</div>
 	);
 };
