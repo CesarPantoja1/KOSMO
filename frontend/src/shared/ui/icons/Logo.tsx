68e078c8078586cc2adc28193e887a5ef471bb9e
Jefferson Chileno <jefferson.chileno@epn.edu.ec>
2026-08-26 12:49:08 -0500

    styles: anadir icono de la app

--- frontend/src/shared/ui/icons/Logo.tsx
+++ frontend/src/shared/ui/icons/Logo.tsx
@@ -3,13 +3,15 @@ interface LogoProps {
 	className?: string;
 }
 
+import image from '../../../../public/kosmo.png';
+
 export function Logo({ size = 36, className = '' }: LogoProps) {
 	return (
 		<div
 			className={`flex items-center justify-center rounded-xl bg-linear-to-br from-ai-500 to-ai-600 font-bold text-neutral-0 ${className}`}
 			style={{ width: size, height: size, fontSize: size * 0.44 }}
 		>
-			K
+			<img src={image.src} alt='image' className='bg-transparent' />
 		</div>
 	);
 }
